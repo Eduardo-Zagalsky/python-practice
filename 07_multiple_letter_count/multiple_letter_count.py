903c6a0b6433e9e1e516e5552449e18ae53a732a
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-08-03 19:12:39 -0500

    added multi letter count logic and tested it

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,11 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    letters = {}
+    for ltr in phrase:
+        if ltr in letters:
+            letters[ltr] += 1
+        else:
+            letters[ltr] = 1
+    print(letters)
+        
