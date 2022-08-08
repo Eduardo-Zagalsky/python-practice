d6cc84c8be7f8239e7f14ffd87f674a1d3b091bf
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-08-08 10:22:40 -0500

    added extract name logic and tested it

--- 21_extract_full_name/extract_full_name.py
+++ 21_extract_full_name/extract_full_name.py
@@ -14,3 +14,7 @@ def extract_full_names(people):
         >>> extract_full_names(names)
         ['Ada Lovelace', 'Grace Hopper']
     """
+    names = []
+    for lst in people:
+        names.append(f"{lst['first']} {lst['last']}")
+    return names
